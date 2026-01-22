db8ae574cdec8f18fca0c7d08464f5b039908481
Founder-ReceiptIt <founder@receiptit.co.uk>
2026-01-22 15:18:55 +0000

    Font fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['JetBrains Mono', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'monospace', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
